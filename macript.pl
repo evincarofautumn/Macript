39b3572bfa34c23fe4e4ad8e12fa62e73fafde4d
Jon Purdy <evincarofautumn@gmail.com>
2011-09-12 11:23:46 -0400

    Added POD.

--- macript.pl
+++ macript.pl
@@ -1,14 +1,42 @@
 #!/usr/bin/perl
-#
-# macript 1.0
-# By Jon Purdy
-#
-# Searches source files for anything that looks like a macro invocation
-# and replaces them with the result of invoking the corresponding
-# script with the given arguments, if such a script exists in the
-# directory where macript is invoked. Results are sent to standard
-# output, presumably for piping to a compiler or interpreter.
-#
+
+=head1 NAME
+
+Macript - Replaces macros with the output of scripts.
+
+=head1 SYNOPSIS
+
+    macript.pl source.cpp > target.cpp
+
+=head1 DESCRIPTIOIN
+
+Searches source files for anything that looks like a macro invocation (C<NAME(PARAMS)>) and replaces them with the result of invoking the corresponding executable with the given arguments, if such an executable exists in the directory where B<macript> is invoked. Results are sent to standard output, presumably for piping to a compiler or interpreter.
+
+=head1 CAVEATS
+
+Be very careful about what executables you have in your working directory when running this, and verify the output you get before it breaks your build; if you're crazy enough to use this in a makefile, you're going to have to use, e.g.:
+
+    macript $< | gcc -x c++ - -o $@
+
+=head1 COPYRIGHT
+
+Copyright (c) 2011 Jon Purdy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+=head1 AVAILABILITY
+
+http://github.com/evincarofautumn/Macript
+
+=head1 AUTHOR
+
+Jon Purdy <evincarofautumn@gmail.com>
+
+=cut
 
 use warnings;
 use strict;
