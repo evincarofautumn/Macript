5d33d671aa577a51726bedf4f9a2adb879b857a0
Jon Purdy <evincarofautumn@gmail.com>
2011-09-12 13:50:22 -0400

    Added various options and improved documentation.

--- macript.pl
+++ macript.pl
@@ -1,22 +1,122 @@
 #!/usr/bin/perl
 
+use warnings;
+use strict;
+use Getopt::Long;
+use Pod::Usage;
+
+Getopt::Long::Configure('bundling');
+
+my $prefix = '';
+my @extensions = ();
+my $warnings = 0;
+my $force = '';
+my $strip = 0;
+GetOptions("extensions|ext|e:s" => \@extensions,
+           "prefix|p:s" => \$prefix,
+           "warnings|warn|w" => \$warnings,
+           "force|f:s" => \$force,
+           "strip|s" => \$strip) or pod2usage(2);
+@extensions = split(',', join(',', @extensions));
+@extensions = ('', qw(pl py sh)) if (!@extensions);
+
+my $name = qr{[A-Z_][0-9A-Z_]*}i;      # Name (1)
+my $params = qr{(\(                    # Parens and contents (2)
+                 ((?:                  # Just contents (3)
+                   (?>"(?:[^"]|\\")+") # Quoted string
+                   |(?>[^()]+)         # Non-parens (no backtracking)
+                   |(?2))*)\))}ox;     # Nested parens (2)
+
+my $macro = qr{\Q$prefix\E($name)$params};
+
+while (<>) {
+	while (/$macro/g) {
+		my ($n, $a) = ($1, $3);
+		if (my $s = find($n)) {
+			s|$prefix($n)$params|`./$s $a`|e;
+		} elsif ($strip) {
+			s|$prefix($n)$params||;
+		} elsif ($warnings) {
+			print STDERR <<"EOF";
+$ARGV:$.: Warning: Macro "$prefix$n" has no match.
+$ARGV:$.: Note: Use --strip to erase macros without matches.
+EOF
+		}
+	}
+	print;
+}
+
+sub find {
+	my $n = shift;
+	my @t = map {"$n$_"} map {$_ eq '' ? '' : ".$_"} @extensions;
+	my @g = grep {-e && -x _} @t;
+	my @h = @g;
+	return undef if (scalar(@g) == 0);
+	if (scalar(@g) > 1 && $force ne '') {
+		@h = grep { /.$force$/ } @g;
+	}
+	if (scalar(@h) > 1) {
+		die <<"EOF";
+$ARGV:$.: Error: Macro \"$prefix$n\" is ambiguous.
+$ARGV:$.: Note: Candidates are: @g
+EOF
+	} elsif (scalar(@h) == 0) {
+		die<<"EOF";
+$ARGV:$.: Error: Macro \"$prefix$n\" is ambiguous.
+$ARGV:$.: Note: No match can be forced with --force $force
+$ARGV:$.: Note: Candidates are: @g
+EOF
+	}
+	return $h[0];
+}
+
+__END__
+
 =head1 NAME
 
-Macript - Replaces macros with the output of scripts.
+Macript - A build tool for invoking scripts like macros.
 
 =head1 SYNOPSIS
 
-    macript.pl source.cpp > target.cpp
+    macript.pl [OPTIONS] SOURCES...
+
+=head2 Options
+
+=over 4
+
+=item B<--extensions>=I<LIST>, B<--ext>=I<LIST>, B<-e> I<LIST>
+
+Specify a comma-separated list of script extensions to search for (e.g., B<-e pl>). Defaults to B<--extensions=pl,py,sh,>.
+
+=item B<--prefix> I<PREFIX>, B<-p> I<PREFIX>
+
+Specify a prefix to require on all macro invocations (e.g., B<-p @>). Defaults to empty.
+
+=item B<--warnings>, B<--warn>, B<-w>
+
+Print warnings to standard error when there is no matching executable for what appears to be a macro invocation.
+
+=item B<--force> I<EXTENSION>, B<-f> I<EXTENSION>
+
+Specify the extension to prefer if multiple executables match a macro.
+
+=item B<--strip>, B<-s>
+
+Erase invocations that have no matching executable, rather than leaving them in place.
+
+=back
 
 =head1 DESCRIPTIOIN
 
-Searches source files for anything that looks like a macro invocation (C<NAME(PARAMS)>) and replaces them with the result of invoking the corresponding executable with the given arguments, if such an executable exists in the directory where B<macript> is invoked. Results are sent to standard output, presumably for piping to a compiler or interpreter.
+B<Macript> searches input sources for anything that looks like a macro invocation (C<NAME(PARAMS)>). It then replaces those invocations with the result of invoking the corresponding executable with the given arguments, if such an executable exists in the directory where B<macript> is invoked. Results are sent to standard output, presumably for piping to a compiler or interpreter.
 
 =head1 CAVEATS
 
-Be very careful about what executables you have in your working directory when running this, and verify the output you get before it breaks your build; if you're crazy enough to use this in a makefile, you're going to have to use, e.g.:
+Be I<extremely> careful about what executables you have in your working directory when running this, and verify the output you get before it breaks your build; if you're crazy enough to use this in a makefile, you're going to have to use, e.g.:
 
-    macript $< | gcc -x c++ - -o $@
+    macript -e pl -p @ $< | gcc -x c++ - -o $@
+
+I I<strongly recommend> that you require a prefix. The only reason there is no default prefix is so that Macript may be conveniently used with many languages.
 
 =head1 COPYRIGHT
 
@@ -37,35 +137,3 @@ http://github.com/evincarofautumn/Macript
 Jon Purdy <evincarofautumn@gmail.com>
 
 =cut
-
-use warnings;
-use strict;
-
-$#ARGV == 0 or die "Usage: macript.pl FILE\n";
-open(FILE, $ARGV[0]) or die "Unable to open $ARGV[0]: $!\n";
-
-my $name = qr{[A-Z_][0-9A-Z_]*}i;       # Name (1)
-my $params = qr{(\(                     # Parentheses and contents (2)
-                 ((?:                   # Just contents (3)
-                   (?>"(?:[^"]|\\")+")  # Quoted string
-                   |(?>[^()]+)          # Non-parentheses (no backtracking)
-                   |(?2))*)\))}ox;      # Nested parentheses (2)
-
-my $macro = qr{($name)$params}i;
-
-while (<FILE>) {
-	while (/$macro/g) {
-		my ($n, $a) = ($1, $3);
-		# print STDERR ">>> Name=\"$n\", Params=\"$a\"\n";
-		if (my $s = find($n)) { s|($n)$params|`./$s $a`|e };
-	}
-	print;
-}
-
-close FILE;
-
-sub find {
-	my $n = shift;
-	my @t = map {"$n$_"} ('', qw(.pl .py .sh));
-	(grep {-e && -x _} @t)[0];
-}
